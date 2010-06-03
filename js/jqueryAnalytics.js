c28982322cff58a744fc2d1ea3da062109985cfe
Balan Vlad <cybyvlad@gmail.com>
2010-06-03 22:42:05 +0300

    Added tracking pixel js

---
+++ js/jqueryAnalytics.js
@@ -0,0 +1,25 @@
+var DEFINED_STATS_QUEUE_MAX_LENGTH = 1;
+var statsQueue = new Array();
+function addEventToQueue(eventName, eventValue) {
+    statsQueue.push({en:eventName,ev:eventValue});
+    if (statsQueue.length >= DEFINED_STATS_QUEUE_MAX_LENGTH)
+    {
+        sendDataToServer();
+    }
+}
+
+function sendDataToServer() {
+    var queryString = "";
+    var queItem = statsQueue.pop();
+    var i = 0;
+    queryString += "?en" + i + "=" + queItem.en;
+    queryString += "&ev" + i + "=" + queItem.ev;
+    i++;
+    while (statsQueue.length > 0) {
+        queItem = statsQueue.pop();
+        queryString += "?en" + i + "=" + queItem.en;
+        queryString += "&ev" + i + "=" + queItem.ev;
+        i++;
+    }
+    $("<img>").attr("src", "http://localhost" + queryString);
+}
