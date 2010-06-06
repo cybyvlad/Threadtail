a15d90d0b1fa2db98a20e5a9137d5a81176766db
Balan Vlad <cybyvlad@gmail.com>
2010-06-06 16:42:40 +0300

    Added timestamp to all messages sent within the querystring

--- js/jqueryAnalytics.js
+++ js/jqueryAnalytics.js
@@ -1,27 +1,34 @@
 var SERVICE_URL = "http://localhost";
 
-var DEFINED_STATS_QUEUE_MAX_LENGTH = 1;
+var DEFINED_STATS_QUEUE_MAX_LENGTH = 3;
 var statsQueue = new Array();
 function addEventToQueue(eventName, eventValue) {
-    statsQueue.push({en:eventName,ev:eventValue});
+    statsQueue.push({en:eventName,ev:eventValue,t:new Date().getTime()});
     if (statsQueue.length >= DEFINED_STATS_QUEUE_MAX_LENGTH)
     {
         sendDataToServer();
     }
 }
-
 function sendDataToServer() {
     var queryString = "";
-    var queItem = statsQueue.pop();
+    var queueItem = statsQueue.pop();
     var i = 0;
-    queryString += "?en" + i + "=" + queItem.en;
-    queryString += "&ev" + i + "=" + queItem.ev;
-    i++;
+    queryString += "?en" + i + "=" + queueItem.en;
+    queryString += "&ev" + i + "=" + queueItem.ev;
+    queryString += "&t" + i + "=" + queueItem.t;
+	    i++;
     while (statsQueue.length > 0) {
-        queItem = statsQueue.pop();
-        queryString += "?en" + i + "=" + queItem.en;
-        queryString += "&ev" + i + "=" + queItem.ev;
+        queueItem = statsQueue.pop();
+        queryString += "&en" + i + "=" + queueItem.en;
+        queryString += "&ev" + i + "=" + queueItem.ev;
+        queryString += "&t" + i + "=" + queueItem.t;
         i++;
     }
     $("<img>").attr("src", SERVICE_URL + queryString);
 }
+
+(function($){
+   $.watch = function(argument) {
+		alert(argument);
+   }
+})(jQuery)
