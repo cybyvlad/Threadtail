55b4bec85e6862320c138ff44f42ef93168cbb8b
Balan Vlad <cybyvlad@gmail.com>
2010-06-20 09:30:38 +0300

    changed gitignore file and created batch that compiles both solutions

---
+++ makeall.bat
@@ -0,0 +1,5 @@
+cd MessagesProcessor
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe MessagesProcessor.sln 
+cd ..
+cd Server
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ThreadtailServer.sln
